9911522742365328b846fc8c60ea2579980e1eca
quixlar <141807330+quixlar@users.noreply.github.com>
2023-09-07 09:22:19 -0700

    css updared

--- Landing Page/CSS/java.js
+++
@@ -1,7 +0,0 @@
-function openForm() {
-    document.getElementById("signup").style.display = "block";
-  }
-  
-  function closeForm() {
-    document.getElementById("signup").style.display = "none";
-  }
---
+++ Landing Page/java.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("open-signup").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("open-signup").style.display = "none";
+  }
